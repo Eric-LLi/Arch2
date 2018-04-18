dc22158ef67e4f9d95f5fd70c569cb1f0744cb70
Eric-LLi <ericli1116@gmail.com>
2018-04-18 17:37:56 +1000

    Replace function of upload photos

--- HomeAccessJS/htmlGeneralFunctions.js
+++ HomeAccessJS/htmlGeneralFunctions.js
@@ -732,304 +732,113 @@ var tradeClear = function (tid) {
     mirror.value = "";
 };
 
+//Upload photos button to trigger input file.
+$("#uploadImg_Btn").click(function (){
+    $("#Imgs_Upload").trigger("click");
+});
+var photos_count = 0;
 
-function HAUploadImages() {
-    document.getElementById('HAUploadImages').click();
-}
+$("#Imgs_Upload").change(function (e){
+    //Store how many files user has selected.
+    const totalFiles = e.currentTarget.files;
+    console.log("File number: "+ totalFiles.length);
 
-$('#HAUploadImages').change(function () {
-    $("#HAImagesDIV").empty();
-    var table = document.getElementById("HAImagesTable");
-    table.style.display = 'block';
-    var count = this.files.length;
-    var imageFile = this.files;
+    //Only action when user has selected file.
+    if(totalFiles.length>0){
+        //Show load document message.
+        $("#Img-loader").show();
 
-    for (var i = 0; i < count; i++) {
-        try {
-            //noinspection ExceptionCaughtLocallyJS
-            throw i
-        } catch (ii) {
-            setTimeout(function () {
-                var nameID = ii + 1;
-                var altName = 'HAImage ' + nameID;
-                var imageID = 'HAImage' + ii;
-                var textID = 'HAImageText' + ii;
-                var removeButtonID = 'HAImageRemoveButton' + ii;
-                var addButtonID = 'HAImageAddButton' + ii;
-                var uploadID = 'HAImageUpload' + ii;
+        //Loop each file.
+        Object.keys(totalFiles).forEach(i => {
+            const file = totalFiles[i];
+            $("#Img-loader").hide();
+            //Show load image message
+            $("#Imgpage-loader").show();
 
-                //var removeFunction = 'RemoveDilapidationImage' + ii + '()';
-                //addDrawing();
-                addImageElements(altName, imageID, textID, removeButtonID, addButtonID, uploadID,
-                                 'removeOneHAImage(this.id)', 'addOneHAImage(this.id)', '400px', '400px');
+            //Create elements for img.
+            //element[imgContainerID, newImgID, imgTextID, imgRmBtnID]
+            const element = createImgs(photos_count);
+            photos_count++;
 
-                loadImage.parseMetaData(imageFile[ii], function (data) {
+            //FileReader() only support new version of browser.
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                var code = reader.result;
+                if(code!==null){
+                    setTimeout(function(){
+                        if(("#"+element[1]).length>0){
+                            $("#"+element[1]).attr("src",code);
 
-                    var orientation = 0;
-                    var date = new Date();
-                    var imageName = imageFile[ii].name;
-                    var imageType = imageFile[ii].type;
-                    var image = document.getElementById(imageID);
-                    var removeButton = document.getElementById(removeButtonID);
-                    var description = document.getElementById(textID);
-                    var addButton = document.getElementById(addButtonID);
-                    //if exif data available, update orientation
-                    if (data.exif) {
-                        orientation = data.exif.get('Orientation');
-                    }
-                    var loadingImage = loadImage(imageFile[ii], function (canvas) {
-                        var base64data = canvas.toDataURL(imageType);
-                        //var img_src = base64data.replace(/^data\:image\/\w+\;base64\,/, '');
-                        image.setAttribute('src', base64data);
-                        //$(selectionImage).attr('src',base64data);
-                        removeButton.style.display = 'block';
-                        removeButton.style.width = '400px';
-                        addButton.style.display = 'none';
-                        addButton.style.width = '400px';
-                        description.style.display = 'block';
-                        image.style.display = 'block';
-                        image.style.width = '400px';
-                        image.style.height = '400px';
-                        description.style.width = '400px';
-                        // image.style.height = '250px';
-                        var file = new File([convertBase64UrlToBlob(base64data, imageType)], imageName, {
-                            type: imageType,
-                            lastModified: date.getTime()
-                        });
+                            var imgFile = new File([convertBase64UrlToBlob(code, file.type)], file.name, {
+                                type: file.type,
+                                lastModified: file.lastModifiedDate
+                            });
 
-                        doUploadFile(file, imageID, textID, removeButtonID, addButtonID, 'HAImagesTable', altName, 'HAImagesDIV', uploadID, 'removeOneHAImage(this.id)', 'addOneHAImage(this.id)', '400px', '400px');
-                    }, {
-                        canvas: true,
-                        orientation: orientation,
-                        maxWidth: 1500,
-                        maxHeight: 1200
-                    });
-                });
-            }, 200);
-        }
+//                            doUploadFile(imgFile,element[1],element[2]);
+                        }
+                        else{
+                            console.log("NO element::" + element[1]);
+                        }
+                    },30);
+                }
+                else{
+                    console.log("No code for image!!");
+                }
+            }
+            reader.readAsDataURL(file);
+        })
+        $("#Imgpage-loader").hide();
+        $("#HA_ImgsContents").show();
     }
-    setTimeout(function () {
-        //addDrawing();
-        var altID = count + 1;
-        var altName = 'Image' + altID;
-        var imageID = 'HAImage' + count;
-        var textID = 'HAImageText' + count;
-        var removeButtonID = 'HAImageRemoveButton' + count;
-        var addButtonID = 'HAImageAddButton' + count;
-        var uploadID = 'HAImageUpload' + count;
-        addImageElements(altName, imageID, textID, removeButtonID, addButtonID, uploadID,
-                         'removeOneHAImage(this.id)', 'addOneHAImage(this.id)', '400px', '400px');
-    }, 400)
 });
 
-function removeOneHAImage(click_id) {
-    var selectedID = String(click_id);
-    var id = selectedID.replace(/[^\d.]/g, '');
-    var imageID = 'HAImage' + id;
-    var removeButtonID = 'HAImageRemoveButton' + id;
-    var addButtonID = 'HAImageAddButton' + id;
-    var descriptionID = 'HAImageText' + id;
+//Photos page; create html image, text, remove button and container.
+function createImgs(id){
+    var imgContainer = document.createElement("div"),
+        newImg = document.createElement("img"),
+        imgText = document.createElement("input"),
+        imgRmBtn = document.createElement("button"),
+        lastContainer = document.getElementById("HA_ImgsContents"),
+        imgContainerID = photos_count + "_imgContainer",
+        newImgID = photos_count + "_AddImg",
+        imgTextID = photos_count + "_imgText",
+        imgRmBtnID = photos_count + "_imgRmBtn";
 
-    var imageSelect = '#' + imageID;
-    $(imageSelect).attr('src', '#');
-    var image = document.getElementById(imageID);
-    var button = document.getElementById(removeButtonID);
-    var addButton = document.getElementById(addButtonID);
-    var description = document.getElementById(descriptionID);
+    //Setting element's attribute.
+    imgContainer.setAttribute("id",imgContainerID);
 
-    button.style.display = 'none';
-    addButton.style.display = 'block';
-    description.value = "";
-    description.style.display = 'none';
-    //image.style.width = '0px';
-    image.style.display = 'none';
-    doRemovePhoto(imageID);
-}
+    newImg.setAttribute("id",newImgID);
 
-function addOneHAImage(click_id) {
-    console.log(click_id);
+    imgText.setAttribute("id", imgTextID);
+    imgText.setAttribute("type", "text");
+    imgText.setAttribute("class", "form-control");
 
-    var id;
-    var selectedID = String(click_id);
-    id = selectedID.replace(/[^\d.]/g, '');
-    var nameID = Number(id) + 1;
-    var altName = 'Image ' + nameID;
-    var imageID = 'HAImage' + id;
-    var textID = 'HAImageText' + id;
-    var removeButtonID = 'HAImageRemoveButton' + id;
-    var addButtonID = 'HAImageAddButton' + id;
-    var uploadID = 'HAImageUpload' + id;
-    console.log(uploadID);
-    var x = document.getElementById(uploadID);
-    x.click();
-    x.addEventListener('change', function () {
-        if (this.files && this.files[0]) {
-            var imageFile = this.files[0];
-            //load the image src to the current imageID.
-            loadImage.parseMetaData(imageFile, function (data) {
-                //console.log('I am in loadImage function');
-                var orientation = 0;
-                var date = new Date();
-                // var selectionImage = '#AdviceImage' + ii;
-                var imageName = imageFile.name;
-                var imageType = imageFile.type;
-                var image = document.getElementById(imageID);
-                var removeButton = document.getElementById(removeButtonID);
-                var description = document.getElementById(textID);
-                var addButton = document.getElementById(addButtonID);
-                //if exif data available, update orientation
-                if (data.exif) {
-                    orientation = data.exif.get('Orientation');
-                }
-                var loadingImage = loadImage(imageFile, function (canvas) {
-                    var base64data = canvas.toDataURL(imageType);
-                    //var img_src = base64data.replace(/^data\:image\/\w+\;base64\,/, '');
-                    image.setAttribute('src', base64data);
-                    //$(selectionImage).attr('src',base64data);
-                    removeButton.style.display = 'block';
-                    removeButton.style.width = '400px';
-                    addButton.style.display = 'none';
-                    description.style.display = 'block';
-                    image.style.display = 'block';
-                    image.style.width = '400px';
-                    image.style.height = '400px';
-                    // image.style.height = '250px';
-                    var file = new File([convertBase64UrlToBlob(base64data, imageType)], imageName, {
-                        type: imageType,
-                        lastModified: date.getTime()
-                    });
-                    //console.log(file);
-                    doUploadFile(file, imageID, textID, removeButtonID, addButtonID, 'HAImagesTable', altName, 'HAImagesDIV', uploadID, 'removeHAHOWImage(this.id)', 'addOneHAImage(this.id)', '400px', '400px');
+    imgRmBtn.setAttribute("id",imgRmBtnID);
+    imgRmBtn.setAttribute("class","btn btn-danger");
 
-                }, {
-                    canvas: true,
-                    orientation: orientation,
-                    maxWidth: 1500,
-                    maxHeight: 1200
-                });
-            });
-        }
+    lastContainer.appendChild(imgContainer);
+    document.getElementById(imgContainerID).appendChild(newImg);
+    document.getElementById(imgContainerID).appendChild(imgText);
+    document.getElementById(imgContainerID).appendChild(imgRmBtn);
+
+    var elements = [imgContainerID, newImgID, imgTextID, imgRmBtnID];
+
+    $("#"+imgRmBtnID).html("Remove");
+    //Photos images remove button listerner.
+    $("#"+imgRmBtnID).click(function (){
+        imagesRemoveBtn(imgContainerID);
     });
-
-    var newID = $('#HAImagesDIV').find('> form').length;
-    var altID = $('#HAImagesDIV').find('> form').length + 1;
-    nextAltName = 'Image  ' + altID;
-    console.log("I am here!!! need another image element ,the next id  " + newID);
-    var nextImageID = 'HAImage' + newID;
-    var nextTextID = 'HAImageText' + newID;
-    var nextRemoveButtonID = 'HAImageRemoveButton' + newID;
-    var nextAddButtonID = 'HAImageAddButton' + newID;
-    var nextUploadID = 'HAImageUpload' + newID;
-    addImageElements(nextAltName, nextImageID, nextTextID, nextRemoveButtonID, nextAddButtonID, nextUploadID,
-                     'removeOneHAImage(this.id)', 'addOneHAImage(this.id)', '400px', '0px');
+    return elements;
 }
 
-//add an image element into the <form>, need a divID, imageID, imageTextID, uploadID, removeID
-function addImageElements(imageAltName, imageID, imageTextID, removeButtonID, addButtonID, uploadFileID, removeFunction, addFunction, imageSize, width) {
-    var BigContainer = document.getElementById('HAImagesDIV');
-
-    //var BigContainer = document.getElementById(divID);
-    var form = document.createElement("form");
-    // console.log(form);
-    //form.setAttribute("class","divForm");
-    //need four dividends in a form
-    var container1 = document.createElement("div");
-    var container2 = document.createElement("div");
-    var container3 = document.createElement("div");
-    var container4 = document.createElement("div");
-    container1.setAttribute("class", "col-sm");
-    container2.setAttribute("class", "col-sm");
-    container3.setAttribute("class", "col-sm");
-    container4.setAttribute("class", "col-sm");
-
-    //crate an image area
-    var img = document.createElement('img');
-    img.src = "#";
-    img.alt = imageAltName;
-    img.id = imageID;
-    img.style.display = 'none';
-    img.style.marginLeft = '10px';
-    img.style.width = width;
-    img.style.height = imageSize;
-    img.style.paddingTop = '10px';
-
-    //create an input for the text
-    var textInput = document.createElement('INPUT');
-    textInput.setAttribute("type", "text");
-    textInput.style.width = imageSize;
-    textInput.style.marginLeft = '10px';
-    textInput.style.height = "10px";
-    textInput.style.display = 'none';
-    textInput.id = imageTextID;
-
-    //create an input for the remove button
-    var removeButton = document.createElement('INPUT');
-    removeButton.setAttribute("type", "button");
-    removeButton.setAttribute("value", "Remove");
-    removeButton.setAttribute("onclick", removeFunction);
-    removeButton.id = removeButtonID;
-    //removeButton.onclick = removeFunction;
-    removeButton.style.width = imageSize;
-    removeButton.style.marginLeft = '10px';
-    removeButton.style.height = "25px";
-    removeButton.style.display = "none";
-
-    //create an input for add button
-    var addButton = document.createElement('INPUT');
-    addButton.setAttribute("type", "button");
-    addButton.setAttribute("value", "Add");
-    addButton.setAttribute("onclick", addFunction);
-    addButton.id = addButtonID;
-    addButton.style.width = imageSize;
-    addButton.style.marginLeft = '10px';
-    addButton.style.height = "25px";
-    addButton.style.display = 'block';
-    addButton.style.marginTop = '20px';
-
-    //create an input for file, to upload images, this is the one with upload action
-    var uploadFile = document.createElement('INPUT');
-    uploadFile.setAttribute("type", "file");
-    uploadFile.id = uploadFileID;
-    uploadFile.setAttribute("class", "inputImage");
-    uploadFile.setAttribute("accept", "image/x-png,image/jpeg");
-
-    uploadFile.style.display = 'none';
-
-    //put all elements into the correct container
-    //BigContainer.appendChild(form);
-    BigContainer.appendChild(form);
-    form.appendChild(container1);
-    form.appendChild(container2);
-    form.appendChild(container3);
-    form.appendChild(container4);
-    container1.appendChild(img);
-    container2.appendChild(textInput);
-    container3.appendChild(removeButton);
-    container4.appendChild(addButton);
-    container4.appendChild(uploadFile);
-}
-
-//Source from http://www.blogjava.net/jidebingfeng/articles/406171.html
-function convertBase64UrlToBlob(urlData, type) {
-
-    var bytes = window.atob(urlData.split(',')[1]); //remove url, convert to byte
-
-    //deal with anomaly, change the ASCI code less than = 0 to great than zero
-    var ab = new ArrayBuffer(bytes.length);
-    var ia = new Uint8Array(ab);
-    for (var i = 0; i < bytes.length; i++) {
-        ia[i] = bytes.charCodeAt(i);
-    }
-
-    return new Blob([ab], {
-        type: type
-    });
+//Photos page; images remove button action.
+function imagesRemoveBtn(id){
+    var container = $("#"+id).remove();
 }
 
 var __PDF_DOC,
     __CANVAS = $('#pdf-canvas').get(0),
     __CANVAS_CTX = __CANVAS.getContext('2d');
-
 function showPDF(pdf_url) {
     $("#pdf-loader").show();
 
@@ -1051,11 +860,9 @@ function showPDF(pdf_url) {
     });
 }
 
-//<img id="0_IMG" width="200" height="300"><br>
-//<button id="0_imgDelete" type="button" class="btn btn-danger" onclick="deleteImg(this.id);">Remove</button>
-
+//Count Sketches images number.
 var imageCount = 0;
-
+//Create Sketches elements to save images.
 function addImgBtn(id) {
     //Create element
     var imgElement = document.createElement("img"),
@@ -1098,7 +905,7 @@ function addImgBtn(id) {
     return combine;
 }
 
-//image delete button onclick event
+//Sketches image delete button onclick event
 // bid = button ID
 var deleteImg = function (bid) {
     var imageID = bid.split("_")[0] + "_IMG",
@@ -1173,7 +980,6 @@ function showPage(page_no) {
 }
 
 // Upon click this should should trigger click on the #file-to-upload file input element
-// This is better than showing the not-good-looking file input element
 $("#upload-button").on('click', function () {
     $("#file-to-upload").trigger('click');
 });
@@ -1197,6 +1003,18 @@ $("#file-to-upload").on('change', function () {
         showPDF(URL.createObjectURL(uploadPDFfile[i]));
     }
 });
+
+//Source from http://www.blogjava.net/jidebingfeng/articles/406171.html
+function convertBase64UrlToBlob(urlData,type){
+    var bytes = window.atob(urlData.split(',')[1]);        //remove url, convert to byte
+    //deal with anomaly, change the ASCI code less than = 0 to great than zero
+    var ab = new ArrayBuffer(bytes.length);
+    var ia = new Uint8Array(ab);
+    for (var i = 0; i < bytes.length; i++) {
+        ia[i] = bytes.charCodeAt(i);
+    }
+    return new Blob( [ab] , {type : type});
+}
 
 $(document).ready(function () {
     "use strict";
