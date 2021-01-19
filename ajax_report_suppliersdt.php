a569f95930a9dea4f7d01efe605fc9d1e8dee5b0
Fafa Lai <fafalai@users.noreply.github.com>
2021-01-20 01:52:01 +1100

    AA-154

--- ajax_report_suppliersdt.php
+++ ajax_report_suppliersdt.php
@@ -17,6 +17,8 @@
       $reports = [];
       $df = explode(" ", $datefrom);
       $dt = explode(" ", $dateto);
+      $datefrom = $datefrom . " 00:00:00";
+      $dateto = $dateto . " 23:59:59";
       $headings = '"Date Approved","Booking #","Type","Name","Address","Amount","Tax","Paid"';
       //
       $filename = "";
@@ -33,7 +35,7 @@
       $supertotaltax = 0.0;
       $supertotalpaid = 0.0;
 
-      $dbselect = "select b1.id,b1.itype,date_format(b1.dateapproved, '%d/%m/%Y') dateapproved,b1.commission,b1.travel,b1.spotter,b1.custfirstname,b1.custlastname,b1.custemail,b1.custaddress1,b1.custaddress2,b1.custcity,b1.custpostcode,b1.custstate,u1.uuid,u1.firstname memberfirstname,u1.lastname memberlastname,u1.gstinc from bookings b1 left join users u1 on (b1.users_id=u1.id) where b1.dateapproved is not null and b1.datecancelled is null and b1.dateclosed is null and b1.dateexpired is null and b1.datecreated between '$datefrom' and '$dateto' and u1.uuid in ('$members') order by u1.firstname,u1.lastname,b1.id";
+      $dbselect = "select b1.id,b1.itype,date_format(b1.dateapproved, '%d/%m/%Y') dateapproved,b1.commission,b1.travel,b1.spotter,b1.custfirstname,b1.custlastname,b1.custemail,b1.custaddress1,b1.custaddress2,b1.custcity,b1.custpostcode,b1.custstate,u1.uuid,u1.firstname memberfirstname,u1.lastname memberlastname,u1.gstinc from bookings b1 left join users u1 on (b1.users_id=u1.id) where b1.dateapproved is not null and b1.datecancelled is null and b1.dateclosed is null and b1.dateexpired is null and b1.dateapproved between '$datefrom' and '$dateto' and u1.uuid in ('$members') order by u1.firstname,u1.lastname,b1.id";
       error_log($dbselect);
       if ($dbresult = SharedQuery($dbselect, $dblink))
       {
@@ -48,7 +50,7 @@
             $superfilename = "./tmp/Suppliers_" . $df[0] . "-" . $dt[0] . ".csv";
             $reports[] = $superfilename;
             $mysuperfile = fopen($superfilename, "w");
-
+            // error_log($mysuperfile);
             if ($mysuperfile !== false)
             {
               fwrite($mysuperfile, '"Member",' . $headings);
@@ -56,25 +58,27 @@
 
               foreach ($rows as $row)
               {
+                error_log("start to write");
                 $membername = str_replace(" ", "_", $row['memberfirstname']) . "_" . str_replace(" ", "_", $row['memberlastname']);
                 $spotter = $row['spotter'];
                 $commission = $row['commission'];
                 $travel = $row['travel'];
 
                 // Start new file for each member
+                error_log($previousuuid);
                 if ($previousuuid != $row['uuid'])
                 {
+                  error_log("start individual member");
                   $previousuuid = $row['uuid'];
+                  //error_log($myfile);
                   if ($myfile !== false)
                   {
                     fwrite($myfile, '"","","","","Total","$' . number_format($totalamount, 2) . '","$' . number_format($totaltax, 2) . '","$' . number_format($totalpaid, 2) . '"');
-
-
-
                     fclose($myfile);
                   }
 
                   $filename = "./tmp/" . $membername . "_" . $df[0] . "-" . $dt[0] . ".csv";
+                  error_log($filename);
                   $reports[] = $filename;
                   $myfile = fopen($filename, "w");
 
@@ -116,15 +120,19 @@
                               '"$' . number_format($tax, 2) . '",' .
                               '"$' . number_format($paid, 2) . '"' .
                               '';
-
+                  //Write to each member report
                   fwrite($myfile, $lineitem);
                   fwrite($myfile, "\n");
+                  //Write to completed report
+                  fwrite($mysuperfile, '"' . $row['memberfirstname'] . " " . $row['memberlastname'] . '",' . $lineitem);
+                  fwrite($mysuperfile, "\n");
 
                   $spotter = floatval($spotter);
                   $travel = floatval($travel);
 
                   if ($spotter > 0.0)
                   {
+                    error_log("has spotter fee need to add another row to the report,both completed and individual");
                     $tax = ($row['gstinc'] == 1) ? $spotter * 0.1 : 0.0;
                     $paid = $spotter + $tax;
 
@@ -148,10 +156,14 @@
 
                     fwrite($myfile, $lineitem);
                     fwrite($myfile, "\n");
+
+                    fwrite($mysuperfile, '"' . $row['memberfirstname'] . " " . $row['memberlastname'] . '",' . $lineitem);
+                    fwrite($mysuperfile, "\n");
                   }
 
                   if ($travel > 0.0)
                   {
+                    error_log("has traveller fee need to add another row to the report, both completed and indivdual");
                     $tax = ($row['gstinc'] == 1) ? $travel * 0.1 : 0.0;
                     $paid = $travel + $tax;
 
@@ -175,19 +187,23 @@
 
                     fwrite($myfile, $lineitem);
                     fwrite($myfile, "\n");
+
+                    fwrite($mysuperfile, '"' . $row['memberfirstname'] . " " . $row['memberlastname'] . '",' . $lineitem);
+                    fwrite($mysuperfile, "\n");
                   }
                 }
 
-                fwrite($mysuperfile, '"' . $row['memberfirstname'] . " " . $row['memberlastname'] . '",' . $lineitem);
-                fwrite($mysuperfile, "\n");
+                
               }
 
+              //The final rwo for the each member report. 
               if ($myfile !== false)
               {
                 fwrite($myfile, '"","","","","Total","$' . number_format($totalamount, 2) . '","$' . number_format($totaltax, 2) . '","$' . number_format($totalpaid, 2) . '"');
                 fclose($myfile);
               }
 
+              //The final row for the full suppliers report. 
               fwrite($mysuperfile, '"","","","","","Total","$' . number_format($supertotalamount, 2) . '","$' . number_format($supertotaltax, 2) . '","$' . number_format($supertotalpaid, 2) . '"');
               fclose($mysuperfile);
 
